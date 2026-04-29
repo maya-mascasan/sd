96ffd8e9bb937fd821afb8ee1715feb6f526872f
maya_mascasan <mascasan.se.maya@student.utcluj.ro>
2026-04-29 16:37:32 +0300

    update

--- demo_backend/src/main/java/com/andrei/demo/service/EmailService.java
+++ demo_backend/src/main/java/com/andrei/demo/service/EmailService.java
@@ -11,20 +11,21 @@ public class EmailService {
 
     private final JavaMailSender mailSender;
 
-    public void sendResetCode(String toEmail, String code) {
+    public void sendResetCode(String to, String code) {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(toEmail);
+        message.setTo(to);
         message.setSubject("Password Reset Code");
         message.setText("Your password reset code is: " + code +
-                "\nThis code expires in 15 minutes.");
+                "\nThis code will expire in 15 minutes.");
         mailSender.send(message);
     }
 
-    public void sendPasswordChangedConfirmation(String toEmail) {
+    public void sendPasswordChangedConfirmation(String to) {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(toEmail);
-        message.setSubject("Password Changed Successfully");
-        message.setText("Your password was recently changed. If this was not you, please contact support immediately.");
+        message.setTo(to);
+        message.setSubject("Security Alert: Password Changed");
+        message.setText("Your password was recently updated. " +
+                "If you did not make this change, please contact us immediately.");
         mailSender.send(message);
     }
 }
